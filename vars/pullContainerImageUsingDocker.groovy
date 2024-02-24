dd550f94787df4c6b7ed15931702af003e37656e
marcoshssilva <76234459+marcoshssilva@users.noreply.github.com>
2024-02-24 00:04:32 -0300

    feat: Create script to pull image on registry

---
+++ vars/pullContainerImageUsingDocker.groovy
@@ -0,0 +1,20 @@
+#!/usr/bin/env groovy
+
+def call(String image, String tag, Map<String, String> params = [:]) {
+    def executableDockerBin = params.containsKey('executableDockerBin') ? params['executableDockerBin'] : 'docker'
+    def mirror = params.containsKey('mirror') ? params['mirror'] ? 'docker.io/'
+
+    def baseCommandTag  = "${executableDockerBin} tag ${image}:${tag} ${mirror}/${image}:${tag}"
+    def baseCommandPull = "${executableDockerBin} pull ${mirror}/${image}:${tag}"
+    def baseCommandRmi  = "${executableDockerBin} rmi ${mirror}/${image}:${tag}"
+
+    if (isUnix()) {
+        sh "${baseCommandTag}"
+        sh "${baseCommandPull}"
+        sh "${baseCommandRmi}"
+    } else {
+        bat "${baseCommandTag}"
+        bat "${baseCommandPull}"
+        bat "${baseCommandRmi}"
+    }
+}

914abd19f21c61fa26ebfa40c08dac6bfb34f8a2
marcoshssilva <76234459+marcoshssilva@users.noreply.github.com>
2024-02-23 23:45:17 -0300

    refactor: Refactor code from buildContainerImageUsingDocker

--- vars/buildContainerImageUsingDocker.groovy
+++ vars/buildContainerImageUsingDocker.groovy
@@ -1,43 +1,14 @@
 #!/usr/bin/env groovy
 
-/**
- Example how to use:
-
- pipeline {
-    agent any
-    stages {
-        stage('') {
-            steps {
-                git branch: 'main', url: 'https://github.com/example/image.git' 
-                script {
-                    // with params
-                    buildContainerImageUsingDocker('teste', 'latest', '.', ['executableDockerBin': '/usr/local/bin/docker', 'contextPath': './'])
-                    // simple mode
-                    buildContainerImageUsingDocker('teste', 'latest', '.')
-                }
-            }
-        }
-    }
-}
- */
 def call(String image, String tag, Map<String, String> params = [:]) {
-    // param 'executableDockerBin' -> docker binary executable
-    String baseCommand = params.any{k -> k.key == 'executableDockerBin'} ? "${params['executableDockerBin']} build -t ${image}:${tag} " : "docker build -t ${image}:${tag} "
-    
-    // param 'contextPath' -> working directory to run build
-    if (params.any{k -> k.key == 'contextPath'}) {
-        baseCommand += "${params['contextPath']} "
-    } else {
-        baseCommand += '.'
-    }
-    
-    
-    // execute command 
+    def executableDockerBin = params.containsKey('executableDockerBin') ? params['executableDockerBin'] : 'docker'
+    def contextPath = params.containsKey('contextPath') ? params['contextPath'] : '.'
+
+    def baseCommand = "${executableDockerBin} build -t ${image}:${tag} ${contextPath}"
+
     if (isUnix()) {
-        // linux or mac os
         sh "${baseCommand}"
     } else {
-        // windows
         bat "${baseCommand}"
     }
 }

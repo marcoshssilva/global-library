cf3c5c6e554396625337f7d277abcbf25f39893e
marcoshssilva <76234459+marcoshssilva@users.noreply.github.com>
2024-02-24 00:10:35 -0300

    feat: fix trailling

--- vars/execSystemPruneUsingDocker.groovy
+++ vars/execSystemPruneUsingDocker.groovy
@@ -3,10 +3,10 @@
 def call(Boolean deleteAll, Boolean deleteVolumes, Map<String, String> params = [:]) {
     
     def executableDockerBin = params.containsKey('executableDockerBin') ? params['executableDockerBin'] : 'docker'
-    def paramDeleteAll = deleteAll ? '--all' : ''
-    def paramDeleteVolumes = deleteVolumes ? '--volumes' : ''
+    def paramDeleteAll = deleteAll ? ' --all' : ''
+    def paramDeleteVolumes = deleteVolumes ? ' --volumes' : ''
 
-    def commandToRun = "${executableDockerBin} system prune ${paramDeleteAll} ${paramDeleteVolumes}"
+    def commandToRun = "${executableDockerBin} system prune${paramDeleteAll}${paramDeleteVolumes}"
     if (isUnix()) {
         sh "${commandToRun}"
     } else {

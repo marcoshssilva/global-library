d1822b979863df0714617d5d1c1052f2934237c5
marcoshssilva <76234459+marcoshssilva@users.noreply.github.com>
2024-02-24 00:09:26 -0300

    feat: Create script to call system prune

---
+++ vars/execSystemPruneUsingDocker.groovy
@@ -0,0 +1,16 @@
+#!/usr/bin/env groovy
+
+def call(Boolean deleteAll, Boolean deleteVolumes, Map<String, String> params = [:]) {
+    
+    def executableDockerBin = params.containsKey('executableDockerBin') ? params['executableDockerBin'] : 'docker'
+    def paramDeleteAll = deleteAll ? '--all' : ''
+    def paramDeleteVolumes = deleteVolumes ? '--volumes' : ''
+
+    def commandToRun = "${executableDockerBin} system prune ${paramDeleteAll} ${paramDeleteVolumes}"
+    if (isUnix()) {
+        sh "${commandToRun}"
+    } else {
+        bat "${commandToRun}"
+    }
+
+}

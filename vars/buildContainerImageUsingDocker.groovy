b8c85b46823d2d3a94898c9400c2d019eb856c28
SenhorDasEstrelas443 <76234459+marcoshssilva@users.noreply.github.com>
2024-02-25 18:23:20 -0300

    feat: add param --platform to def buildContainerImageUsingDocker

--- vars/buildContainerImageUsingDocker.groovy
+++ vars/buildContainerImageUsingDocker.groovy
@@ -3,8 +3,9 @@
 def call(String image, String tag, Map<String, String> params = [:]) {
     def executableDockerBin = params.containsKey('executableDockerBin') ? params['executableDockerBin'] : 'docker'
     def contextPath = params.containsKey('contextPath') ? params['contextPath'] : '.'
+    def platform = params.containsKey('platform') ? "--platform ${params['platform']}" : ''
 
-    def baseCommand = "${executableDockerBin} build -t ${image}:${tag} ${contextPath}"
+    def baseCommand = "${executableDockerBin} build -t ${image}:${tag} ${platform} ${contextPath}"
 
     if (isUnix()) {
         sh "${baseCommand}"

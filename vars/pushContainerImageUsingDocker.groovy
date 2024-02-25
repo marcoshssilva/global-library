3589b674491509262d968cbef17c4ccdb757a44a
SenhorDasEstrelas443 <76234459+marcoshssilva@users.noreply.github.com>
2024-02-25 20:09:35 -0300

    fix: fixed mirror when not passed but image already contains

--- vars/pushContainerImageUsingDocker.groovy
+++ vars/pushContainerImageUsingDocker.groovy
@@ -2,11 +2,11 @@
 
 def call(String image, String tag, Map<String, String> params = [:]) {
     def executableDockerBin = params.containsKey('executableDockerBin') ? params['executableDockerBin'] : 'docker'
-    def mirror = params.containsKey('mirror') ? params['mirror'] : 'docker.io/'
+    def mirror = params.containsKey('mirror') ? params['mirror'] : ''
 
-    def baseCommandTag  = "${executableDockerBin} tag ${image}:${tag} ${mirror}/${image}:${tag}"
-    def baseCommandPull = "${executableDockerBin} push ${mirror}/${image}:${tag}"
-    def baseCommandRmi  = "${executableDockerBin} rmi ${mirror}/${image}:${tag}"
+    def baseCommandTag  = "${executableDockerBin} tag ${image}:${tag} ${mirror}${image}:${tag}"
+    def baseCommandPull = "${executableDockerBin} push ${mirror}${image}:${tag}"
+    def baseCommandRmi  = "${executableDockerBin} rmi ${mirror}${image}:${tag}"
 
     if (isUnix()) {
         sh "${baseCommandTag}"

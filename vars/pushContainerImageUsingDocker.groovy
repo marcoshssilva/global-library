b6410effb78c2e0a60b05cc0955eecccc62475c6
SenhorDasEstrelas443 <76234459+marcoshssilva@users.noreply.github.com>
2024-02-25 20:10:35 -0300

    fix: fixed missing character when pass mirror

--- vars/pushContainerImageUsingDocker.groovy
+++ vars/pushContainerImageUsingDocker.groovy
@@ -2,7 +2,7 @@
 
 def call(String image, String tag, Map<String, String> params = [:]) {
     def executableDockerBin = params.containsKey('executableDockerBin') ? params['executableDockerBin'] : 'docker'
-    def mirror = params.containsKey('mirror') ? params['mirror'] : ''
+    def mirror = params.containsKey('mirror') ? params['mirror'] + '/' : ''
 
     def baseCommandTag  = "${executableDockerBin} tag ${image}:${tag} ${mirror}${image}:${tag}"
     def baseCommandPull = "${executableDockerBin} push ${mirror}${image}:${tag}"

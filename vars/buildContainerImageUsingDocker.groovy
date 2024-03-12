d519b9596bd334a03f98282c73f7c69ae6ab1fcb
SenhorDasEstrelas443 <76234459+marcoshssilva@users.noreply.github.com>
2024-03-11 22:52:28 -0300

    refactor: host to include flag -H only if needed

--- vars/buildContainerImageUsingDocker.groovy
+++ vars/buildContainerImageUsingDocker.groovy
@@ -6,9 +6,9 @@ def call(String image, String tag, Map<String, String> params = [:]) {
     def platform = params.containsKey('platform') ? "--platform ${params['platform']}" : ''
     def becomeSudo = params.getOrDefault('becomeSudo', 'false') == 'true' ? 'sudo ' : ''
     def options = params.getOrDefault('options', '')
-    def host = params.getOrDefault('host', '/var/run/docker.sock')
+    def host = params.containsKey('host') ? '-H ${host}' : ''
 
-    def baseCommand = "${becomeSudo}${executableDockerBin} -H ${host} build ${options} -t ${image}:${tag} ${platform} ${contextPath}"
+    def baseCommand = "${becomeSudo}${executableDockerBin} ${host} build ${options} -t ${image}:${tag} ${platform} ${contextPath}"
 
     if (isUnix()) {
         sh "${baseCommand}"

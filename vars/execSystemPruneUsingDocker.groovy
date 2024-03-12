973f8e910fcfcf1a1ec92de4d30c7f05a100048c
SenhorDasEstrelas443 <76234459+marcoshssilva@users.noreply.github.com>
2024-03-11 22:53:32 -0300

    Update execSystemPruneUsingDocker.groovy

--- vars/execSystemPruneUsingDocker.groovy
+++ vars/execSystemPruneUsingDocker.groovy
@@ -6,9 +6,10 @@ def call(Boolean deleteAll, Boolean deleteVolumes, Map<String, String> params = 
     def paramDeleteAll = deleteAll ? ' --all' : ''
     def paramDeleteVolumes = deleteVolumes ? ' --volumes' : ''
     def becomeSudo = params.getOrDefault('becomeSudo', 'false') == 'true' ? 'sudo ' : ''
+    def host = params.containsKey('host') ? '-H ${host}' : ''
 
-    def commandToRun = "echo Y | ${becomeSudo}${executableDockerBin} system prune${paramDeleteAll}${paramDeleteVolumes}"
-    def commandPrintUsages = "${becomeSudo}${executableDockerBin} system df"
+    def commandToRun = "echo Y | ${becomeSudo}${executableDockerBin} ${host} system prune${paramDeleteAll}${paramDeleteVolumes}"
+    def commandPrintUsages = "${becomeSudo}${executableDockerBin} ${host} system df"
     if (isUnix()) {
         sh "${commandToRun}"
     } else {

34bde7ba7f6e6082f4e0c5cd2cc79e6094a4d275
marcoshssilva <76234459+marcoshssilva@users.noreply.github.com>
2024-04-07 14:46:23 -0300

    feat: add support to TLS

--- vars/pushContainerImageUsingDocker.groovy
+++ vars/pushContainerImageUsingDocker.groovy
@@ -5,10 +5,13 @@ def call(String image, String tag, Map<String, String> params = [:]) {
     def mirror = params.containsKey('mirror') ? params['mirror'] + '/' : ''
     def becomeSudo = params.getOrDefault('becomeSudo', 'false') == 'true' ? 'sudo ' : ''
     def host = params.containsKey('host') ? "-H ${params['host']}" : ''
+    def useTls = params.containsKey('useTls') && params['useTls'] ? ' --tls' : ''
+    def tlsCertificate = params.containsKey('tlsCertificate') ? " --tlskey=${params['tlsCertificate']}" : ''
+    def tlsPrivateKey  = params.containsKey('tlsPrivateKey')  ? " --tlskey=${params['tlsPrivateKey']}"  : ''
 
-    def baseCommandTag  = "${becomeSudo}${executableDockerBin} ${host} tag ${image}:${tag} ${mirror}${image}:${tag}"
-    def baseCommandPull = "${becomeSudo}${executableDockerBin} ${host} push ${mirror}${image}:${tag}"
-    def baseCommandRmi  = "${becomeSudo}${executableDockerBin} ${host} rmi ${mirror}${image}:${tag}"
+    def baseCommandTag  = "${becomeSudo}${executableDockerBin}${useTls}${tlsCertificate}${tlsPrivateKey} ${host} tag ${image}:${tag} ${mirror}${image}:${tag}"
+    def baseCommandPull = "${becomeSudo}${executableDockerBin}${useTls}${tlsCertificate}${tlsPrivateKey} ${host} push ${mirror}${image}:${tag}"
+    def baseCommandRmi  = "${becomeSudo}${executableDockerBin}${useTls}${tlsCertificate}${tlsPrivateKey} ${host} rmi ${mirror}${image}:${tag}"
 
     if (isUnix()) {
         if (mirror != '') {

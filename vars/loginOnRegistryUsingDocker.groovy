b5ac444171a18e26246a20031a822baaaf1cb55b
marcoshssilva <76234459+marcoshssilva@users.noreply.github.com>
2024-04-07 14:46:15 -0300

    feat: add support to TLS

--- vars/loginOnRegistryUsingDocker.groovy
+++ vars/loginOnRegistryUsingDocker.groovy
@@ -3,7 +3,10 @@
 def call(String registryUrl, String credentialsId, String executablePath = 'docker') {
     withCredentials([usernamePassword(credentialsId: credentialsId, usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]) {
         def host = params.containsKey('host') ? "-H ${params['host']}" : ''
-        def command = "echo \$PASSWORD | $executablePath ${host} login $registryUrl --username \$USERNAME --password-stdin"
+        def useTls = params.containsKey('useTls') && params['useTls'] ? ' --tls' : ''
+        def tlsCertificate = params.containsKey('tlsCertificate') ? " --tlskey=${params['tlsCertificate']}" : ''
+        def tlsPrivateKey  = params.containsKey('tlsPrivateKey')  ? " --tlskey=${params['tlsPrivateKey']}"  : ''
+        def command = "echo \$PASSWORD | $executablePath ${host}${useTls}${tlsCertificate}${tlsPrivateKey} login $registryUrl --username \$USERNAME --password-stdin"
         if (isUnix()) { 
             sh command
         } else { 
